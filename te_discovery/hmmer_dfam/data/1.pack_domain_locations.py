0eb7e9b57b73224ba1a50ef60c4033519f861fb5
oaxiom <oaxiom@gmail.com>
2019-05-22 11:57:14 +0800

    speed up te_discovery/hmmer_dfam/data/1.pack_domain_locations.py

--- te_discovery/hmmer_dfam/data/1.pack_domain_locations.py
+++ te_discovery/hmmer_dfam/data/1.pack_domain_locations.py
@@ -11,7 +11,7 @@ After [(280, 574), (281, 361), (547, 689), (671, 995), (913, 994), (1061, 1349),
 
 import glob, sys, os, gzip
 from operator import itemgetter
-from glbase3 import utils, expression, genelist, glload
+from glbase3 import utils, expression, genelist, glload, config
 
 dfam = genelist('dfam_annotation.tsv', format={'force_tsv': True, 'name': 0, 'HMMlength': 1, 'species': 2, 'type': 3, 'subtype': 4})
 
@@ -37,7 +37,7 @@ for f in sorted(glob.glob('raw_data/tblout*.tsv.gz')):
     skipped = 0
     transcript_table = {}
     sam = os.path.split(f)[1].replace('.tsv', '').replace('.gz', '').replace('domtblout.', '').replace('tblout.', '')
-    print('Working %s' % sam)
+    config.log.info('Working %s' % sam)
     oh = gzip.open(f, 'rt')
 
     res_te_count = {}
@@ -49,7 +49,7 @@ for f in sorted(glob.glob('raw_data/tblout*.tsv.gz')):
 
         dom_name = line[2]
 
-        species = dfam.get(key='name', value=dom_name)['species'][0]
+        species = dfam._findDataByKeyLazy(key='name', value=dom_name)['species']
         #print(species)
 
         if species in reject_species: # Only keep Hominid TEs
@@ -65,66 +65,74 @@ for f in sorted(glob.glob('raw_data/tblout*.tsv.gz')):
         #print(transcript_table[line[0]])
 
         if n % 1000000 == 0:
-            print('Processed: {:,} domains'.format(n))
-            #break
+            config.log.info('Processed: {:,} domains'.format(n))
+            #if n == 2e6: break
     oh.close()
 
-    print('Skipped {:,} non-Hominid TEs'.format(skipped))
+    config.log.info('Skipped {:,} non-Hominid TEs'.format(skipped))
 
     # Save summary table
     # Can use a glbase, although saving it as a TSV would be a mess
 
     # Why does this part take so long?
-    print('Removing duplicate domains in {:,} transcripts'.format(len(transcript_table)))
-    # remove duplicate, overlapping doms; take the + strand, best scoring domains if there are any overlaps;
+    config.log.info('Removing duplicate domains in {:,} transcripts'.format(len(transcript_table)))
+    # remove duplicate, overlapping doms; take best scoring domains if there are any overlaps;
+
     for idx, transcript_id in enumerate(transcript_table):
         transcript = transcript_table[transcript_id]
+        #print('Before:', len(transcript['doms']))
 
         if len(transcript['doms']) <= 1:
             continue
         #all_spans = [d for d in transcript['doms']]
-        marked_for_deletion = []
-        for idx_dom1, dom1 in enumerate(transcript['doms']):
+        # Perform the simple-case and delete identical domains, keep the lowest_scoring E value;
+        all_doms = list(set([(d['E'], (d['span'][0], d['span'][1]), d['strand'], d['dom']) for d in transcript['doms']])) # remove exactly identical (should be none?)
+        all_doms = sorted(all_doms, key=itemgetter(0)) # E value first
+        all_doms = sorted(all_doms, key=itemgetter(1)) # then span
+        new_doms = []
+        spans_seen = set([])
+        for d in all_doms: # now delete dupes based on span, and the order they appear
+            if d[1] not in spans_seen:
+                new_doms.append(d)
+                spans_seen.add(d[1])
+        all_doms = new_doms
+        #print('After remove simple Dupes:', len(all_doms))
+
+        # This is slow, so obvious cases are deleted before entering;
+        marked_for_deletion = set([])
+        for idx_dom1, dom1 in enumerate(all_doms):
             if dom1 in marked_for_deletion: # if already marked for deletion; don't do it again, in case of ties
                 continue
-            for idx_dom2, dom2 in enumerate(transcript['doms']):
-                if idx_dom1 >= idx_dom2: # do a triangular match, so that you only compare each pair once;
-                    continue
+
+            for idx_dom2, dom2 in enumerate(all_doms):
+                if idx_dom2 >= idx_dom1: # do a triangular match, so that you only compare each pair once;
+                    break
 
                 if dom2 in marked_for_deletion:# if already marked for deletion; don't do it again, in case of ties
                     continue
 
-                dom1_span = dom1['span']
-                dom2_span = dom2['span']
-                # see if there is an overlap
-                # Check for the simple case where two domains are identical:
-                if dom1_span[0] == dom2_span[0] and dom1_span[1] == dom2_span[1]:
-                    if dom1['E'] <= dom2['E']:
-                        marked_for_deletion.append(dom2)
-                        continue
-                    else:
-                        marked_for_deletion.append(dom1)
-                        continue
+                dom1_span = dom1[1]
+                dom2_span = dom2[1]
 
                 # The second simple case: One domain is entirely contained in another domain
-                if (dom1_span[0] > dom2_span[0] and dom1_span[1] < dom2_span[1]): # dom1 is inside dom2:
-                    if dom1['E'] <= dom2['E']:
-                        marked_for_deletion.append(dom2)
+                if dom1_span[0] > dom2_span[0] and dom1_span[1] < dom2_span[1]: # dom1 is inside dom2:
+                    if dom1[0] <= dom2[0]:# E value
+                        marked_for_deletion.add(dom2)
                         continue
                     else:
-                        marked_for_deletion.append(dom1)
+                        marked_for_deletion.add(dom1)
                         continue
 
-                if (dom2_span[0] > dom1_span[0] and dom2_span[1] < dom1_span[1]): # dom2 is inside dom1:
-                    if dom1['E'] <= dom2['E']:
-                        marked_for_deletion.append(dom2)
+                if dom2_span[0] > dom1_span[0] and dom2_span[1] < dom1_span[1]: # dom2 is inside dom1:
+                    if dom1[0] <= dom2[0]:# E value
+                        marked_for_deletion.add(dom2)
                         continue
                     else:
-                        marked_for_deletion.append(dom1)
+                        marked_for_deletion.add(dom1)
                         continue
 
                 # The third complex case, check the percentage overlap:
-                if (dom1_span[1] > dom2_span[0] and dom1_span[0] < dom2_span[1]): # Has an overlap;
+                if dom1_span[1] > dom2_span[0] and dom1_span[0] < dom2_span[1]: # Has an overlap;
                     # get number of base pairs overlapping:
                     bp_overlap = min(abs(dom1_span[1] - dom2_span[0]), abs(dom1_span[0] - dom2_span[1]))
 
@@ -133,28 +141,25 @@ for f in sorted(glob.glob('raw_data/tblout*.tsv.gz')):
                     #print(dom1['span'], dom2['span'], bp_overlap, perc_overlap)
                     if perc_overlap > 0.6:
                         # Whichever one has the lower E value:
-                        if dom1['E'] <= dom2['E']:
-                            marked_for_deletion.append(dom2)
+                        if dom1[0] <= dom2[0]: # E value
+                            marked_for_deletion.add(dom2)
                             continue
                         else:
-                            marked_for_deletion.append(dom1)
+                            marked_for_deletion.add(dom1)
                             continue
 
-                        marked_for_deletion.append(dom2) # choose a random one;
-
         if marked_for_deletion: # Sometimes none;
-            #print('Before', sorted([d['span'] for d in transcript['doms']], key=itemgetter(0)))
-            marked_for_deletion = list(map(dict, frozenset(frozenset(i.items()) for i in marked_for_deletion)))
             for m in marked_for_deletion:
-                transcript_table[transcript_id]['doms'].remove(m)
-
-            #print()
-            #print('After', sorted([d['span'] for d in transcript['doms']], key=itemgetter(0)))
-            #print()
-            #break
+                all_doms.remove(m)
+        transcript_table[transcript_id]['doms'] = [{'E': d[0], 'span': d[1], 'strand': d[2], 'dom': d[3]} for d in all_doms] # Put it back as a dict;
+        #print('Final:', len(transcript_table[transcript_id]['doms']))
+        #print('Final set:', sorted([d['span'] for d in transcript_table[transcript_id]['doms']], key=itemgetter(0)))
+        #print()
+        #break
 
         if (idx+1) % 1000 == 0:
-            print('Processed: {:,} transcripts'.format(idx+1))
+            config.log.info('Processed: {:,} transcripts'.format(idx+1))
+            #break
 
     #print(transcript_table)
 
