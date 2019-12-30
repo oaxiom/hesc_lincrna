a4063e34c439e99edd357ea1489da398d396d33a
oaxiom <oaxiom@gmail.com>
2019-12-30 15:55:16 +0800

    download mass spec

---
+++ massspec/raw_data/download_data.sh
@@ -0,0 +1,8 @@
+
+wget -c ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/06/PXD003903/hipsci.proteomics.maxquant.uniprot.MQ1_MQ2.20151023.tar.gz
+
+tar xvzf hipsci.proteomics.maxquant.uniprot.MQ1_MQ2.20151023.tar.gz
+gzip *.txt
+rm hipsci.proteomics.maxquant.uniprot.MQ1_MQ2.20151023.tar.gz
+
+
