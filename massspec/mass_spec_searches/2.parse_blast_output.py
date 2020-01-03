ec94bf3bc708d4761944f881e8af10181f8b966b
oaxiom <oaxiom@gmail.com>
2020-01-03 08:54:33 +0800

    collect the MS pileups

--- massspec/mass_spec_searches/2.parse_blast_output.py
+++ massspec/mass_spec_searches/2.parse_blast_output.py
@@ -19,14 +19,19 @@ for filename in glob.glob('../blast_searches/masked/*.glb'):
 
     res_peps = []
     res_fastas  = {} # for each fasta, a
-    for peptide_fragment in ms_data:
+    p = progressbar(len(ms_data))
+    for idx, peptide_fragment in enumerate(ms_data):
         #print(peptide_fragment)
         for f in fasta:
             if peptide_fragment['seq'] in f['seq']:
-                posl = peptide_fragment['seq'].find(f['seq'])
-                posr = peptide_fragment['seq'].find(f['seq']) + len(peptide_fragment['seq'])
+                posl = f['seq'].find(peptide_fragment['seq'])
+                posr = posl + len(peptide_fragment['seq'])
 
-                res_peps.append({'peptide_fragment': peptide_fragment,
+                res_peps.append({
+                    'name': f['name'],
+                    'seq_length': len(f['seq']),
+                    'seq': f['seq'],
+                    'peptide_fragment': peptide_fragment['seq'],
                     'context': f['seq'][max(posl-5, 0):posl].lower() + f['seq'][posl:posr] + f['seq'][posr:posr+5].lower(),
                     'pos': (posl, posr),
                     'peptide_score': peptide_fragment['score']
@@ -36,18 +41,27 @@ for filename in glob.glob('../blast_searches/masked/*.glb'):
                 if f['name'] not in res_fastas:
                     res_fastas[f['name']] = 0
                 res_fastas[f['name']] += 1
+        p.update(idx)
 
-    resgl = genelist()
-    resgl.load_list(res_peps)
+    if res_peps:
+        resgl = genelist()
+        resgl.load_list(res_peps)
+        resgl.sort('name')
+        resgl.saveTSV('peptide_hits-{0}.tsv'.format(stub), key_order=['name', 'seq_length', 'pos', 'peptide_fragment', 'context', 'peptide_score', 'seq' ])
+        resgl.save('peptide_hits-{0}.glb'.format(stub) )
+        # resgl.saveFASTA
 
-    resgl.saveTSV('peptide_hits-{0}.tsv'.format(stub), key_order=['name', 'blastp_status'])
-    resgl.save('peptide_hits-{0}.glb'.format(stub) )
-    # resgl.saveFASTA
+        # per-transcript number of hits:
+        resgl = genelist()
+        resgl.load_list([{'name': k, 'num_peptides': res_fastas[k]} for k in res_fastas])
+        resgl.sort('name')
+        resgl.saveTSV('per_transcript_hits-{0}.tsv'.format(stub), key_order=['name', 'num_peptides'])
+        #resgl.save('per_transcript_hits-{0}.glb'.format(stub) )
+        print()
+        print(stub)
+        print('Number of matching peptides: {0}'.format(len(resgl)))
+        for k in sorted(res_fastas):
+            print('{0}:\t {1}'.format(k, res_fastas[k]))
+        print()
 
-    print()
-    print(stub)
-    print('Number of matching peptides: {0}'.format(len(resgl)))
-    for k in sorted(res_fastas):
-        print('{0}:\t {1}'.format(k, res_fastas[k]))
-    print()
 
