8db866b644e42561a77f9156d49bad31cc6fc782
oaxiom <oaxiom@gmail.com>
2020-02-28 15:52:28 +0800

    added UMAP plotting of TE-containing transcripts

--- singlecell/scanpy_3ends/te_enrichment/plot_TE_containing/plot_TE-DE.py
+++ singlecell/scanpy_3ends/te_enrichment/plot_TE_containing/plot_TE-DE.py
@@ -1,12 +1,29 @@
 import os, sys, glob, numpy
 import scanpy as sc
 from glbase3 import genelist, glload
+sc.settings.verbosity = 1
+sc.set_figure_params(dpi=200, dpi_save=200)
+sc.settings.figdir = 'markers'
 
+adata = sc.read('../../learned.h5ad')
 
-
-for filename in glob.glob('../*.glb'):
+for filename in glob.glob('../te_containing/*.glb'):
     print(filename)
     # Just do a simple annotation of yh the TE-contianig DE genes from the scRNA-seq
     stub = os.path.split(filename)[1].split('.')[0].replace('de_genes-', '')
     de_list = glload(filename)
 
+    sc.settings.figdir = stub
+
+    for item in de_list:
+        for te in item['doms']:
+            sc.settings.figdir = '{0}/{1}'.format(stub, te['dom'])
+
+            sc.pl.umap(adata,
+                color=item['transcript_id'],
+                size=10,
+                legend_loc='on data',
+                vmax=3,
+                show=False,
+                save='-{0}-markers-{1}-{2}.pdf'.format(te['dom'],  item['transcript_id'], item['name'])
+                )
