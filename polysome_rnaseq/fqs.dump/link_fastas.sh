662b5dfb0e43add3f585de5afef870ad494e0e0d
oaxiom <oaxiom@gmail.com>
2020-12-21 14:51:48 +0800

    added guidance on where to get the SRAs

---
+++ polysome_rnaseq/fqs.dump/link_fastas.sh
@@ -0,0 +1,25 @@
+
+# Download the SRAs and unpack ot make these lines valid:
+
+ln -s fqs/SRR5680873.1_1.fastq.gz hesc_cyto.rp1.p1.fq.gz
+ln -s fqs/SRR5680874.1_1.fastq.gz hesc_nuc.rp1.p1.fq.gz
+ln -s fqs/SRR5680875.1_1.fastq.gz hesc_monosome.rp1.p1.fq.gz
+ln -s fqs/SRR5680876.1_1.fastq.gz hesc_poly_low.rp1.p1.fq.gz
+ln -s fqs/SRR5680877.1_1.fastq.gz hesc_poly_high.rp1.p1.fq.gz
+ln -s fqs/SRR5680888.1_1.fastq.gz hesc_cyto.rp2.p1.fq.gz
+ln -s fqs/SRR5680889.1_1.fastq.gz hesc_nuc.rp2.p1.fq.gz
+ln -s fqs/SRR5680890.1_1.fastq.gz hesc_monosome.rp2.p1.fq.gz
+ln -s fqs/SRR5680891.1_1.fastq.gz hesc_poly_low.rp2.p1.fq.gz
+ln -s fqs/SRR5680892.1_1.fastq.gz hesc_poly_high.rp2.p1.fq.gz
+
+ln -s fqs/SRR5680873.1_2.fastq.gz hesc_cyto.rp1.p2.fq.gz 
+ln -s fqs/SRR5680874.1_2.fastq.gz hesc_nuc.rp1.p2.fq.gz 
+ln -s fqs/SRR5680875.1_2.fastq.gz hesc_monosome.rp1.p2.fq.gz 
+ln -s fqs/SRR5680876.1_2.fastq.gz hesc_poly_low.rp1.p2.fq.gz 
+ln -s fqs/SRR5680877.1_2.fastq.gz hesc_poly_high.rp1.p2.fq.gz 
+ln -s fqs/SRR5680888.1_2.fastq.gz hesc_cyto.rp2.p2.fq.gz 
+ln -s fqs/SRR5680889.1_2.fastq.gz hesc_nuc.rp2.p2.fq.gz 
+ln -s fqs/SRR5680890.1_2.fastq.gz hesc_monosome.rp2.p2.fq.gz 
+ln -s fqs/SRR5680891.1_2.fastq.gz hesc_poly_low.rp2.p2.fq.gz 
+ln -s fqs/SRR5680892.1_2.fastq.gz hesc_poly_high.rp2.p2.fq.gz 
+
