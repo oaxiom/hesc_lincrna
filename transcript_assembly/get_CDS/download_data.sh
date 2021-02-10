ada8a21ca8a8b1704747adcebe7be9637686c484
oaxiom <oaxiom@gmail.com>
2021-02-10 15:12:34 +0800

    added gencode.v32.pc_transcripts.fa.gz to downloads

--- transcript_assembly/get_CDS/download_data.sh
+++ transcript_assembly/get_CDS/download_data.sh
@@ -1,3 +1,4 @@
 
 wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.pc_translations.fa.gz
 
+wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.pc_transcripts.fa.gz
