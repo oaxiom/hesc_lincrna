0baf0da2dff482a1cb8b92dd30863653c5feef44
oaxiom <oaxiom@gmail.com>
2021-02-11 11:41:51 +0800

    added GENCODE expressed canonical CDS to EFJ NMD plot

--- massspec/9.ejc_nmd/1.dist_from_ejc.py
+++ massspec/9.ejc_nmd/1.dist_from_ejc.py
@@ -17,6 +17,9 @@ all_transcripts = glload('../../transcript_assembly/get_CDS/coding_genes_with_lo
 all_exons = glload('../../transcript_assembly/packed/all_genes.glb')
 
 all_transcripts = all_transcripts.map(key='transcript_id', genelist=all_exons)
+all_expressed = all_transcripts.getColumns(['enst', 'transcript_id'])
+all_coding = glload('../../gencode/hg38_gencode_v32.pc.glb').map(genelist=all_transcripts, key='enst')
+print(len(all_coding))
 
 has_peptide_hit = glload('../results_gene.glb').removeDuplicates('transcript_id') # ones with a peptide hit.
 has_peptide_hit = has_peptide_hit.map(genelist=all_transcripts, key='transcript_id')
@@ -26,33 +29,40 @@ no_peptide_hit = has_peptide_hit.map(genelist=no_peptide_hit, key='transcript_id
 no_peptide_hit = no_peptide_hit.map(genelist=all_transcripts, key='transcript_id')
 
 res = {
-    'hit': [],
-    'no hit': []
+    'Canonical': [],
+    'MS Hit': [],
+    'No MS hit': [],
+
     }
     
 data = {
-    'hit': has_peptide_hit,
-    'no hit': no_peptide_hit,
+    'Canonical': all_coding,
+    'MS Hit': has_peptide_hit,
+    'No MS hit': no_peptide_hit,
+
     }
 
 __nostop = 0
+__notfound = 0
 
 for k in data:
     for transcript in data[k]:
         strand = transcript['strand']
         
         cds_key_to_use = None
-        if 'cds_genome_loc' in transcript and transcript['cds_genome_loc']:
+        if 'cds_loc' in transcript and transcript['cds_loc']:
+            cds_key_to_use = 'cds_loc'
+        elif 'cds_genome_loc' in transcript and transcript['cds_genome_loc']:
             cds_key_to_use = 'cds_genome_loc'
         else:
             cds_key_to_use = 'cds_local_to_genome'
         
         if strand == '+':
             STOP = transcript[cds_key_to_use]['right']
-            min_dist_to_ejc = transcript['loc']['right'] - STOP
+            min_dist_to_ejc = 1e9
         else:
             STOP = transcript[cds_key_to_use]['left']
-            min_dist_to_ejc = STOP - transcript['loc']['left']
+            min_dist_to_ejc = 1e9
         #print(STOP, min_dist_to_ejc, transcript)
         
         if STOP == 0:
@@ -65,16 +75,22 @@ for k in data:
                     d = exons[1] - STOP
                 else:
                     d = STOP - exons[0]
-                print(d)
+                #print(d)
                 min_dist_to_ejc = min([min_dist_to_ejc, d])
+        if min_dist_to_ejc == 1e9:
+            __notfound += 1
+            continue
+            
         res[k].append(min_dist_to_ejc)
 
 print('__nostop = {}'.format(__nostop))
-
+print('__notfound = {}'.format(__notfound))
 # percent within 55 bp;
 perc_55 = {    
-    'hit': [0, 0], # <55, total
-    'no hit': [0, 0],
+    'Canonical': [0,0],
+    'No MS hit': [0,0],
+    'MS Hit': [0,0],
+
     }
 qs = {}
 for k in res:
